23f88a47772ff754c4df1bb597fa69c6ca889bc8
Chung Nông Thành <nongthanhchungcbg@gmail.com>
2026-03-05 18:13:31 +0700

    first commit

--- ThucHanhNew/Models/Product.cs
+++ ThucHanhNew/Models/Product.cs
@@ -1,10 +1,16 @@
-﻿namespace ThucHanhNew.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThucHanhNew.Models
 {
     public class Product
     {
+        [Required]
         public int Id { get; set; }
+        [StringLength(100, MinimumLength = 2)]
         public string? Name { get; set; }
-        public decimal Price { get; set; }
         public string? Description { get; set; }
+        [Range(0.01, 10000, ErrorMessage = "Price must be greater than 0 and less than 10000")]
+        public decimal Price { get; set; }
     }
 }
+
